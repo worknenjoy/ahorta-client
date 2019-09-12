065a08fa1c5e88093b654422b55429a5c5d1a5dd
Alexandre Magno <alexanmtz@gmail.com>
2019-09-12 10:20:04 +0200

    hardcoding host

--- src/url.js
+++ src/url.js
@@ -1,2 +1,3 @@
-console.log(process.env.NODE_ENV)
-export const host = process.env.NODE_ENV === 'production' ? 'http://ahorta.herokuapp.com' : ''
+//console.log(process.env.NODE_ENV)
+//export const host = process.env.NODE_ENV === 'production' ? 'http://ahorta.herokuapp.com' : ''
+export const host = 'http://ahorta.herokuapp.com'
