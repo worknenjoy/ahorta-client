a6e8cf5ac167c055406a627575381e0ac5fb7a1a
Alexandre Magno <alexanmtz@gmail.com>
2020-12-27 21:35:44 +0100

    defining one url only to api

--- src/url.js
+++ src/url.js
@@ -1,3 +1,3 @@
 //console.log(process.env.NODE_ENV)
-export const host = process.env.NODE_ENV === 'production' ? 'https://ahorta.herokuapp.com' : 'https://ahorta-testing.herokuapp.com'
-//export const host = 'https://ahorta.herokuapp.com'
+// export const host = process.env.NODE_ENV === 'production' ? 'https://ahorta.herokuapp.com' : 'https://ahorta-testing.herokuapp.com'
+export const host = 'https://ahorta.herokuapp.com'
