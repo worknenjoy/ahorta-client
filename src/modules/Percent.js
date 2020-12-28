4d105080a5b1e79c24e1565f297f4ee7acac52f8
Alexandre Magno <alexanmtz@gmail.com>
2020-12-28 12:03:41 +0100

    fixing percent boundaries

--- src/modules/Percent.js
+++ src/modules/Percent.js
@@ -1,7 +1,12 @@
 const Percent = (value, min, max) => {
   const diff = max - min
   if(diff === 0) return 0
-  return ((value - min)*100/(max - min)).toFixed(2)
+  const result = Math.round((value - min)*100/(max - min))
+  if(result < 0 || result > 100) {
+    return 'wrong input'
+  } else {
+    return result
+  }
 }
 
 export default Percent;
